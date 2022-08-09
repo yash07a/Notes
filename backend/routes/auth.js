de18ee40eae034ca203182273205e266164e8f78
yash07a <yk1539614@gmail.com>
2022-08-09 20:03:47 +0530

    created login endpoint and also jwt to send to user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -50,4 +50,45 @@ router.post(
   }
 );
 
+// authenticating user using post /api/auth/login
+router.post(
+  "/login",
+  [
+    body("email", "invalid email").isEmail(),
+    body("password", "password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: "please try to login with correct details" });
+      }
+      const passswordCompare = await bcrypt.compare(password, user.password);
+      if (!passswordCompare) {
+        return res
+          .status(400)
+          .json({ errors: "please try to login with correct details" });
+      }
+      data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occurred");
+    }
+  }
+);
+
 module.exports = router;
