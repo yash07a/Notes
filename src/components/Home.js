dbcf5753edf20cf4299efb321fd8f4858ea4ccd2
yash07a <yk1539614@gmail.com>
2022-08-16 23:30:33 +0530

    Added notes and noteitem component for displaying notes effectively using bootstrap card component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,6 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/noteContext";
+import Notes from "./Notes";
 
 const Home = () => {
-
-  const context = useContext(noteContext);
-  const { notes, setNotes } = context;
 
   return (
     <div>
@@ -46,12 +42,7 @@ const Home = () => {
           </button>
         </form>
       </div>
-      <div className="container my-3">
-        <h2>Your Notes</h2>
-        {notes.map((note) => {
-          return note.title
-        })}
-      </div>
+      <Notes />
     </div>
   );
 };
