b57fe004d661a8217d58bf66578cd993b0fa49ab
yash07a <yk1539614@gmail.com>
2022-08-17 11:43:34 +0530

    Added alert component and also edit and delete icons in the notes from fontawesome webpage

---
+++ src/components/Alert.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Alert = (props) => {
+
+    /*setTimeout(() => {
+        setmsg(
+            {
+                message: ""
+            }
+        )
+    }, 2000);
+
+    const m = {
+        message: "hi its yash"
+    }
+
+    const [msg, setmsg] = useState(m)*/
+    return (
+        <div>
+            <div class="alert alert-primary" role="alert">
+                {props.message}
+            </div>
+        </div>
+    )
+}
+
+export default Alert
--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -8,6 +8,8 @@ const NoteItem = (props) => {
                 <div class="card-body">
                     <h5 class="card-title">{note.title}</h5>
                     <p class="card-text">{note.description}</p>
+                    <i class="fa-solid fa-pen-to-square mx-2"></i>
+                    <i class="fa-solid fa-trash mx-2"></i>
                 </div>
             </div>
         </div>
